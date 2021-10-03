0db483fb17e5fe8a5d26fab41ad0059b7c768ad6
Mahadev mandal <mandalmahadev5@gmail.com>
2021-10-03 22:35:29 +0545

    changes

--- pages/question/[qid].js
+++ pages/question/[qid].js
@@ -5,23 +5,29 @@ import QuesAns from '../../components/QuesAns';
 import Head from 'next/head';
 import { apiBaseUrl } from '../../helpers/constants';
 
-export async function getStaticPaths() {
-    const res = await axios.get(`${apiBaseUrl}/api/questions/populars`);
-    const popularQues = await res.data
-    const paths = popularQues.map((ques) => ({
-        params: { qid: ques._id }
-    }))
-    return { paths, fallback: false }
-}
+// export async function getStaticPaths() {
+//     const res = await axios.get(`${apiBaseUrl}/api/questions/populars`);
+//     const popularQues = await res.data
+//     const paths = popularQues.map((ques) => ({
+//         params: { qid: ques._id }
+//     }))
+//     return { paths, fallback: false }
+// }
 
-export async function getStaticProps({ params }) {
-    const res = await axios.get(`${apiBaseUrl}/api/question/${params.qid}`)
+// export async function getStaticProps({ params }) {
+//     const res = await axios.get(`${apiBaseUrl}/api/question/${params.qid}`)
+//     const ques = await res.data
+//     return {
+//         props: { ques }
+//     }
+// }
+export async function getServerSideProps({ params }) {
+    const res = await axios.get(`${apiBaseUrl}/api/question/1`)
     const ques = await res.data
     return {
         props: { ques }
     }
 }
-
 
 function Question({ ques }) {
 
