75a03ea1895f7be7b237e3ca6d1e9a007ea940f6
Mahadev mandal <mandalmahadev5@gmail.com>
2021-10-03 22:46:01 +0545

    third commit

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.NODE_ENV === 'production' ? "" : "http://localhost:3000"
+export const apiBaseUrl = process.env.NODE_ENV === 'production' ? "https://kbcqna.vercel.app" : "http://localhost:3000"
